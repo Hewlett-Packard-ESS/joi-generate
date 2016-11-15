7b07dca65c616e071932ff665ec41eef070396f3
Dan <17@dlock.tech>
2016-11-14 21:56:30 -0400

    added contribs, any tests, reformat, cleaned up binary.js

--- test/binary.js
+++ test/binary.js
@@ -4,61 +4,42 @@ var generate = new require('../').Generate();
 require('should');
 
 describe('Object', function() {
+	var go = function(schema, done) {
+		generate(schema, function(err, model) {
+			if(err) {
+				return done(err);
+			}
+			var error = Joi.validate(model, schema);
+			done(error.error);
+		});
+	};
+	
+	it('should generate a binary', function(done) {
+		var schema = Joi.object({
+			file: Joi.binary()
+		});
+		go(schema, done);
+	});
 
-  it('should generate a binary', function(done) {
-    var schema = Joi.object({
-      file: Joi.binary()
-    });
-    generate(schema, function(err, model) {
-      if (err) {
-        return done(err);
-      }
+	it('should generate an ascii encoded binary', function(done) {
+		var schema = Joi.object({
+			file: Joi.binary().encoding('ascii')
+		});
+		go(schema, done);
+	});
 
-      var error = Joi.validate(model, schema);
-      done(error.error);
-    });
-  });
+	it('should generate a binary with minimum', function(done) {
+		var schema = Joi.object({
+			file: Joi.binary().min(10)
+		});
+		go(schema, done);
+	});
 
-  it('should generate an ascii encoded binary', function(done) {
-    var schema = Joi.object({
-      file: Joi.binary().encoding('ascii')
-    });
-    generate(schema, function(err, model) {
-      if (err) {
-        return done(err);
-      }
-
-      var error = Joi.validate(model, schema);
-      done(error.error);
-    });
-  });
-
-  it('should generate a binary with minimum', function(done) {
-    var schema = Joi.object({
-      file: Joi.binary().min(10)
-    });
-    generate(schema, function(err, model) {
-      if (err) {
-        return done(err);
-      }
-
-      var error = Joi.validate(model, schema);
-      done(error.error);
-    });
-  });
-
-  it('should generate a binary with maximum', function(done) {
-    var schema = Joi.object({
-      file: Joi.binary().max(4)
-    });
-    generate(schema, function(err, model) {
-      if (err) {
-        return done(err);
-      }
-
-      var error = Joi.validate(model, schema);
-      done(error.error);
-    });
-  });
+	it('should generate a binary with maximum', function(done) {
+		var schema = Joi.object({
+			file: Joi.binary().max(4)
+		});
+		go(schema, done);
+	});
 
 });
